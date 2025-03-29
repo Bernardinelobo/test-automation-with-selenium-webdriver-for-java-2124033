ec1cc9ca921b67450a1a02739b48ccaccc1871ce
Bernardinelobo <74398129+Bernardinelobo@users.noreply.github.com>
2025-03-29 20:54:53 +0000

    alerts.java

--- src/main/java/browser_interactions/Alerts.java
+++ src/main/java/browser_interactions/Alerts.java
@@ -11,39 +11,46 @@ import setup.Main;
 public class Alerts extends Main {
   
   public static void main(String[] args) {
-    alerts();
-    confirmations();
-    prompts();
+    //alerts();
+    //confirmations();
+    //prompts();
     waits();
 
-    driver.quit();
+    //driver.quit();
   }
 
   public static void alerts(){
     driver.get("https://www.selenium.dev/selenium/web/alerts.html");
     driver.findElement(By.id("alert")).click();
-
+    Alert alert = driver.switchTo().alert();
+    String message = alert.getText();
+    alert.accept();
+    System.out.println(message);
 
   } 
 
   public static void confirmations() {
       driver.get("https://www.selenium.dev/selenium/web/alerts.html");
       driver.findElement(By.id("confirm")).click();
-
-
+      Alert confirmation = driver.switchTo().alert();
+      confirmation.accept();
+      //confirmation.dismiss();
   }
 
   public static void prompts(){
       driver.get("https://www.selenium.dev/selenium/web/alerts.html");
       driver.findElement(By.id("prompt")).click();
-
-
+      Alert prompt = driver.switchTo().alert();
+      prompt.sendKeys("hi");
+      //prompt.accept();
   }
 
   public static void waits(){
       driver.get("https://www.selenium.dev/selenium/web/alerts.html");
       driver.findElement(By.id("slow-alert")).click();
-
+      var wait = new WebDriverWait(driver,Duration.ofSeconds(2));
+      Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+      alert.accept();
 
   }
 }
