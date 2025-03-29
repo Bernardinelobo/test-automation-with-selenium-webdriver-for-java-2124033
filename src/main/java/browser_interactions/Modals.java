755f1bae2b216ef11f896fc38d730c190c49eb03
Bernardinelobo <74398129+Bernardinelobo@users.noreply.github.com>
2025-03-29 19:54:23 +0000

    modals.java

--- src/main/java/browser_interactions/Modals.java
+++ src/main/java/browser_interactions/Modals.java
@@ -1,7 +1,11 @@
 package browser_interactions;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import setup.Main;
 
@@ -11,8 +15,16 @@ public class Modals extends Main {
     driver.get("https://www.selenium.dev/selenium/web/modal_dialogs/modern_modal.html");
     driver.findElement(By.id("trigger-modal-btn")).click();
 
-
-
-    driver.quit();
+    WebElement modal = driver.findElement(By.id("modalContent"));
+    if(modal.isDisplayed())
+    {
+    System.out.println("Modal is displayed successfully");
+    driver.findElement(By.id("modal-input")).sendKeys("Hi modal");
+    driver.findElement(By.id("modal-close")).click();
+    }
+    else{
+      System.out.println("Modal is not displayed");
+    }
+       //driver.quit();
   }
 }
