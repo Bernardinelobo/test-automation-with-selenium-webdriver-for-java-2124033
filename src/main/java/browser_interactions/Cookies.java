f839dae8330c1f394f7b6f3b525d44d41f11039e
Bernardinelobo <74398129+Bernardinelobo@users.noreply.github.com>
2025-03-30 03:58:48 +0000

    cookies.java

--- src/main/java/browser_interactions/Cookies.java
+++ src/main/java/browser_interactions/Cookies.java
@@ -8,9 +8,14 @@ public class Cookies extends Main {
 
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+    Cookie cookie = new Cookie("theme","light");
+    driver.manage().addCookie(cookie);
 
+    Cookie themecookie = driver.manage().getCookieNamed("theme");
+    System.out.println(themecookie.getExpiry());
+    System.out.println(themecookie.getDomain());
+    System.out.println(themecookie.getName());
 
-
-    driver.quit();
+    //driver.quit();
   }
 }
