e93fa7093f1ddf4ff24acd5f06474f681d6b206e
Bernardinelobo <74398129+Bernardinelobo@users.noreply.github.com>
2025-03-31 05:42:54 +0000

    copypaste.java

--- src/main/java/actions/CopyAndPaste.java
+++ src/main/java/actions/CopyAndPaste.java
@@ -11,8 +11,19 @@ public class CopyAndPaste extends Main {
   
   public static void main(String[] args){
     driver.get("https://www.selenium.dev/selenium/web/formPage.html");
+    var copyText = driver.findElement(By.id("withText"));
+    var pasteText = driver.findElement(By.id("emptyTextArea"));
+    Keys cmdctrl = Platform.getCurrent().is(Platform.WINDOWS) ? Keys.COMMAND : Keys.CONTROL;
+    Actions action = new Actions(driver);
+    action.click(copyText)
+    .keyDown(cmdctrl)
+    .sendKeys("a")
+    .sendKeys("c")
+    .keyUp(cmdctrl).perform();
 
-
+    action.click(pasteText)
+    .keyDown(cmdctrl)
+    .sendKeys("v").perform();
 
     //driver.quit();
   }
