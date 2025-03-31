45fe94448c8f30737db0d20a9f7c0614eb5adddc
Bernardinelobo <74398129+Bernardinelobo@users.noreply.github.com>
2025-03-31 03:33:12 +0000

    contextclicking.java

--- src/main/java/actions/ContextClick.java
+++ src/main/java/actions/ContextClick.java
@@ -9,9 +9,9 @@ public class ContextClick extends Main {
   
   public static void main(String[] args) {
     driver.get("https://selenium.dev/selenium/web/mouse_interaction.html");
-        
-
-
-    driver.quit();
+    var input = driver.findElement(By.id("clickable"));
+    new Actions(driver).contextClick(input).perform();
+    System.out.println(driver.findElement(By.id("click-status")).getText());
+    //driver.quit();
   }
 }
