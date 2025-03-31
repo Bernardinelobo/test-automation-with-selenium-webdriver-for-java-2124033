f04e292153d2718e2684b13092bab3e251ac037a
Bernardinelobo <74398129+Bernardinelobo@users.noreply.github.com>
2025-03-31 19:11:41 +0000

    chaleenge_multiplekeypresses.java

--- src/main/java/actions/Challenge_MultipleKeyPresses.java
+++ src/main/java/actions/Challenge_MultipleKeyPresses.java
@@ -1,6 +1,7 @@
 package actions;
 
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.interactions.Actions;
 
 import setup.Main;
@@ -9,9 +10,17 @@ public class Challenge_MultipleKeyPresses extends Main {
 
   public static void main(String[] args) {
     driver.get("https://www.selenium.dev/selenium/web/keyboard_shortcut.html");
+    Keys cmdctrl = Platform.getCurrent().is(Platform.WINDOWS) ? Keys.COMMAND : Keys.CONTROL;
+    Keys cmdshft = Platform.getCurrent().is(Platform.WINDOWS) ? Keys.COMMAND : Keys.SHIFT;
+    Actions action = new Actions(driver);
+    action
+    .keyDown(cmdctrl)
+    .keyDown(cmdshft)
+    .sendKeys("1")
+    .keyUp(cmdctrl)
+    .keyUp(cmdshft)
+    .perform();
 
-
-
-    driver.quit();
+   // driver.quit();
   }
 }
