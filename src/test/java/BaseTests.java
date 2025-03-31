df8bdd0cf9454d232cc6075a95242355e7e6931c
Bernardinelobo <74398129+Bernardinelobo@users.noreply.github.com>
2025-03-31 19:59:40 +0000

    basetests.java

--- src/test/java/BaseTests.java
+++ src/test/java/BaseTests.java
@@ -8,5 +8,22 @@ import static io.github.bonigarcia.wdm.WebDriverManager.chromedriver;
 
 public class BaseTests {
 
+  protected static WebDriver driver;
+
+  @BeforeAll
+    public static void setUp(
+    {
+      chromedriver().setup();
+      ChromeOptions options = new ChromeOptions();
+      options.addArguments("--no-sandbox");// needed for git hub codespaces //
+      driver = new ChromeDriver(options);
+
+    }
+
+    @AfterAll
+    public static void tearDown(){
+      driver.quit();
+    }
+  )
 
 }
